615a75a23790c763c5b8349c0f1a64b7f42a22de
Big anão <103335886+Biganao@users.noreply.github.com>
2022-07-26 23:53:00 -0300

    ATT 0.2
    - Adição de novos arquivos HTML para a criação do crud

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,11 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+//desabilitando envios de formulários se houver campos vazios
 (() => {
     'use strict'
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    // Buscando todos os formulários aos quais aplicamos estilos de validação Bootstrap personalizados
     const forms = document.querySelectorAll('.needs-validation')
   
-    // Loop over them and prevent submission
+    // loop que impede o envio
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
         if (!form.checkValidity()) {
@@ -17,3 +17,67 @@
       }, false)
     })
   })()
+
+// delete de cadastro
+
+function del() {
+  swal({
+    title: "Tem Certeza?",
+    text: "Uma vez deletado, voce não podera recuperar este cadastro",
+    icon: "warning",
+    buttons: ["Cancelar", "Confirmar"],
+    dangerMode: true,
+  })
+  .then((willDelete) => {
+    if (willDelete) {
+      swal("Cadastro removido com sucesso", {
+        icon: "success",
+      });
+      window.location.href = "#" // redirecionameto da pagina
+    } else {
+      swal("Pedido de deletar cadastro, cancelado");
+    }
+  });
+}
+
+// edit de cadastro
+
+function edit() {
+  swal({
+    title: "Tem Certeza?",
+    text: " Voce realmente deseja editar esse cadastro?",
+    icon: "warning",
+    buttons: ["Cancelar", "Confirmar"],
+    dangerMode: true,
+  })
+  .then((Confirm) => {
+    if (Confirm) {
+      window.location.href = "#"; // redirecionameto da pagina
+    } else {
+      swal("Pedido de editar cadastro, cancelado");
+    }
+  });
+}
+
+// criar novo cadastro 
+
+function crea() {
+  swal({
+    title: "Tem Certeza?",
+    text: " Voce realmente deseja criar um novo cadastro",
+    icon: "warning",
+    buttons: ["Cancelar", "Confirmar"],
+    dangerMode: true,
+  })
+  .then((Confirm) => {
+    if (Confirm) {
+      window.location.href = "#"; // redirecionameto da pagina
+    } else {
+      swal("Pedido para criar um novo cadastro, cancelado");
+    }
+  });
+}
+
+
+
+
